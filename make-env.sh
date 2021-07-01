50231b4b50d972c098561da304934c2964c72156
Mikey Garcia <gikeymarcia@gmail.com>
2021-06-30 17:13:20 -0700

    better build script

--- make-env.sh
+++ make-env.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env bash
-# shellcheck disable=SC2086
+# Mikey Garcia, @gikeymarcia
 
-# install dependencies (Ubuntu)
-dev_dep="entr fd-find figlet"
-runtime_dep="fzf rofi dmenu"
-sudo apt install -y $dev_dep $runtime_dep
-
-# install virtualenv package
+env=./env
+if [ -d $env ]; then
+    rm -rf $env
+fi
 python -m pip install --user virtualenv
-
-# destroy and recreate the environment
-rm -rv ./env
-python -m virtualenv env
+python -m virtualenv $env
