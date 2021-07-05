80ddb77589b69dc1e02add2c1770896125976849
Mikey Garcia <gikeymarcia@gmail.com>
2021-07-05 01:50:16 -0700

    reorg tests

--- test/test_fzf.py
+++ test/test_fzf.py
@@ -6,13 +6,6 @@ import pydymenu
 import pytest
 
 
-def test_has_bin():
-    assert pydymenu.has_bin("fzf") == True
-    assert pydymenu.has_bin(None) == False
-    assert pydymenu.has_bin("") == False
-    assert pydymenu.has_bin("printf") == True
-
-
 def test_missing_fzf_error(monkeypatch):
     monkeypatch.setenv("PATH", "")
     with pytest.raises(FileNotFoundError) as except_info:
---
+++ test/test_has_bin.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# Mikey Garcia, @gikeymarcia
+# https://github.com/gikeymarcia/dotfiles
+
+import pydymenu
+import pytest
+from pathlib import Path
+
+
+def test_has_bin():
+    assert pydymenu.has_bin("fzf") == True
+    assert pydymenu.has_bin("rofi") == True
+    assert pydymenu.has_bin("") == False
+    assert pydymenu.has_bin("printf") == True
+
+
+@pytest.mark.parametrize("wrong_types", [None, {}, 127, Path.home()])
+def test_has_bin_throws_value_errors(wrong_types):
+    with pytest.raises(ValueError) as except_info:
+        pydymenu.has_bin(wrong_types)
+    assert except_info.type == ValueError
+
+
+# vim: set foldlevel=2:
